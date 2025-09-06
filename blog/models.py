de3e7204c013bcd4de4a438d210ba3585e38ff53
asim <asimshrestha608@gmail.com>
2025-09-06 15:12:46 +0545

    chore: create canoncial url for post model instance

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -34,3 +35,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", kwargs={"id": self.pk})
