c69c3809f72a24e55d808ef9fccd8ba8c38349c4
asim <asimshrestha608@gmail.com>
2025-09-15 20:54:33 +0545

    feat: recommend similar posts

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from django.db.models import Count
 from taggit.models import Tag
 
 from .models import Post
@@ -41,10 +42,23 @@ def post_detail(request, year, month, day, post):
     )
     comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    # List of similar posts
+    post_tags_id = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_id).exclude(id=post.pk)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
+
     return render(
         request,
         "blog/post/detail.html",
-        {"post": post, "comments": comments, "form": form},
+        {
+            "post": post,
+            "comments": comments,
+            "form": form,
+            "similar_posts": similar_posts,
+        },
     )
 
 
